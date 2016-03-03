c44f219660af12d8fc5332c0d5590c9a1cca8099
munozyanez <munoz.yanez@gmail.com>
2016-03-03 18:22:29 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,13 @@ int main()
     //Setup imu middleware port
     MWI::Port imuPort("/inertial");
     std::stringstream dataIndices, imudata;
+
+    // i = 0,1,2 --> euler angles (deg)
+     // i = 3,4,5 --> linear acceleration (m/s²)
+     // i = 6,7,8 --> angular speed (deg/s)
+     // i = 9,10,11 --> magnetic field (arbitrary units)
     dataIndices << "3 4 5" ;//yarp imu port Linear acceleration in X Y Z [m/s^2]
+
 
     //READ SENSOR
     double a_x,a_y,a_z;
