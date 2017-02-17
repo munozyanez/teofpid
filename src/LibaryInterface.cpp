d0773a2f25d86bfe2a8dd4ac2a9d81298164ab8f
munozyanez <munoz.yanez@gmail.com>
2017-02-17 14:06:37 +0100

    new file:   src/LibaryInterface.cpp 	new file:   src/LibaryInterface.h

---
+++ src/LibaryInterface.cpp
@@ -0,0 +1,6 @@
+#include "LibaryInterface.h"
+
+LibaryInterface::LibaryInterface()
+{
+
+}
---
+++ src/LibaryInterface.h
@@ -0,0 +1,11 @@
+#ifndef LIBARYINTERFACE_H
+#define LIBARYINTERFACE_H
+
+
+class LibaryInterface
+{
+public:
+    LibaryInterface();
+};
+
+#endif // LIBARYINTERFACE_H
