e2b3c8d19aa867bb2487004eba3cbf88241aae49
munozyanez <munoz.yanez@gmail.com>
2017-05-12 16:57:53 +0200

    modified:   CMakeLists.txt 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <time.h>
 #include <fstream>      // std::fstream
 
-#include <yarp/os/all.h>
 
 #include <chrono>
 #include <ctime>
@@ -15,11 +14,7 @@
 #include "IPlot.h"
 
 //local functions
-int velocityCurve(double Ts, double vel, int jointNumber, MWI::Robot& robot, std::vector<double> &pos);
-
-
-//MWI::Robot rightArm(robConfig);
-
+//int velocityCurve(double Ts, double vel, int jointNumber, MWI::Robot& robot, std::vector<double> &pos);
 
 using namespace std;
 
@@ -27,53 +22,20 @@ using namespace std;
 
 int main()
 {
-    //INITIALISE AND CHECK YARP
-    yarp::os::Network yarpNet;
-
-    if ( !yarpNet.checkNetwork(2) )
-    {
-        std::cout << "[error] %s found no YARP network (try running \"yarp detect --write\")." << std::endl;
-        return -1;
-    }
-    else
-    {
-        std::cout << "[success] YARP network found." << std::endl;
-    }
-
-
-
-    //control
 
 
 
     MWI::Robot rightArm("teoSim","rightArm");
+    if (rightArm.GetError()!=0)
+    {
+        std::cout << "MWI::Robot rightArm(\"teoSim\",\"rightArm\") not available. ERROR: " << rightArm.GetError() << std::endl;
+        return -1;
+
+    }
     rightArm.SetControlMode(2);
 
     double Ts = 0.01;
 
-
-    //standard model
-/*
-    double kt=1.09;
-    std::vector<double> motorNum(3,0);
-    motorNum[2]=Ts*Ts;
-    motorNum[1]=2*Ts*Ts;
-    motorNum[0]=Ts*Ts;
-    std::vector<double> motorDen(3,0);
-    motorDen[2]=2*Ts+4*kt;
-    motorDen[1]=-8*kt;
-    motorDen[0]=-2*Ts+4*kt;
-
-*/    //constant acceleration model
-    double km=3;//acceleration
-    std::vector<double> motorNum(3,0);
-    motorNum[0]=km*Ts*Ts;
-    motorNum[1]=km*2*Ts*Ts;
-    motorNum[2]=km*Ts*Ts;
-    std::vector<double> motorDen(3,0);
-    motorDen[0]=4;
-    motorDen[1]=-8;
-    motorDen[2]=4;
 
     //3rd order model sistem id matlab Ts=0.01
 //    std::vector<double> motorNum(4,0);
@@ -87,18 +49,20 @@ int main()
 //    motorDen[1]=2.662;
 //    motorDen[0]=-0.8317;
 
-    SystemBlock model(motorNum,motorDen);
+   // SystemBlock model(motorNum,motorDen);
+
     //plotters
     IPlot pt(Ts),vt(Ts),at(Ts);
     IPlot ptTeo(Ts),vtTeo(Ts),atTeo(Ts);
 
 
-    double ka=1.09;//acceleration
+    double ka=10.09;//acceleration
     //instantiate object motor
     SystemBlock acc(
                 std::vector<double> {ka},
                 std::vector<double> {1}
                 );
+    //graph: acc.SetSaturation(-24,24);
     acc.SetSaturation(-15,15);
 
 
@@ -108,7 +72,8 @@ int main()
                 std::vector<double> {-2,+2}//{Ts-2,Ts+2}
                 );
 
-    vel.SetSaturation(-20,20);
+//    vel.SetSaturation(-5,18);
+    vel.SetSaturation(-5,18);
 
     //instantiate object encoder
     SystemBlock encoder(
@@ -119,45 +84,7 @@ int main()
     double signal,modelSignal,jointPos;
 
 
-
-    //time_t t;
-    double target = 30;
-    double error, modelError;
-    int jointNumber = 3;
-    std::vector<double> realPos(0,0), times(0,0);
-    std::vector<double> modelPos(1,0);
-
-    /*//P control
-
-    std::vector<double> controlNum(1,0);
-    controlNum[0]=1;
-    std::vector<double> controlDen(1,0);
-    controlDen[0]=1;
-*/
-/*     //PID control 5,2,2
-    std::vector<double> controlNum(3,0);
-    controlNum[0]=Ts*Ts-5*Ts+4;
-    controlNum[1]=2*Ts*Ts-8;
-    controlNum[2]=Ts*Ts+5*Ts+4;
-    std::vector<double> controlDen(3,0);
-    controlDen[0]=-Ts;
-    controlDen[1]=0;
-    controlDen[2]=Ts;
-
-
-    std::vector<double> controlNum(2,0);
-    controlNum[0]=-0.5;
-    controlNum[1]=1;
-    std::vector<double> controlDen(2,0);
-    controlDen[0]=+0.5;
-    controlDen[1]=1;
-
-
-    SystemBlock control(controlNum,controlDen);
-    control.SetSaturation(-100,100);
-    SystemBlock controlModel(control);
-*/
-    //PIDBlock control(2,0.5,1,Ts);
+    //old PIDBlock control(2,0.5,1,Ts);
     PIDBlock control(2,0.5,1,Ts);
     PIDBlock controlModel(control);
 
@@ -168,6 +95,12 @@ int main()
 
     rightArm.SetControlMode(2);
 
+    //time_t t;
+    double target = 30;
+    double error, modelError;
+    int jointNumber = 3;
+    std::vector<double> realPos(0,0), times(0,0);
+    std::vector<double> modelPos(1,0);
 
     //control loop
     long loops = 10/Ts;
@@ -191,20 +124,20 @@ int main()
         modelError > controlModel > acc > vel >  encoder;
 
 
-        //plots
+        //plot data
         modelPos.push_back( encoder.GetState() );
         pt.pushBack(encoder.GetState());
         at.pushBack(acc.GetState());
 
 
-        std::cout << times[i]
-                     << " ,real signal: " << signal
-                     << " ,jointPos: " << jointPos
+//        std::cout << times[i]
+//                     << " ,real signal: " << signal
+//                     << " ,jointPos: " << jointPos
 
-                        << ",modelError: " << modelError
-                     << ",modelSignal: " << controlModel.GetState()
-                     << "modelPos: " << encoder.GetState()
-                        << std::endl;
+//                        << ",modelError: " << modelError
+//                     << ",modelSignal: " << controlModel.GetState()
+//                     << "modelPos: " << encoder.GetState()
+//                        << std::endl;
         //std::cout << command << "" << std::endl;
         //yarp::os::Time::delay(Ts);
 
@@ -218,85 +151,74 @@ int main()
 
     rightArm.SetJointVel(jointNumber,0.);
 
-    //rightArm.DefaultPosition();
-    //yarp::os::Time::delay(7);
-/*
-    std::vector curvePositions;
-    velocityCurve(0.01, 20, 3, rightArm, curvePositions);*/
 
-
-    LibraryInterface li;
-
-
-//    li.PlotAndSave(times,realPos,loops*Ts,target*1.5,"realPos.csv");
-//    li.PlotAndSave(times,modelPos,loops*Ts,target*1.5,"modelPos.csv");
 
     return 0;
 }
 
 
-int velocityCurve(double Ts, double vel, int jointNumber, MWI::Robot& robot, std::vector<double> &pos)
-{
-    int loops = 6/Ts;
-    double totalTime=0;
-    double actualTime,lastTime, elapsedTime;
-    pos.clear();
+//int velocityCurve(double Ts, double vel, int jointNumber, MWI::Robot& robot, std::vector<double> &pos)
+//{
+//    int loops = 6/Ts;
+//    double totalTime=0;
+//    double actualTime,lastTime, elapsedTime;
+//    pos.clear();
 
 
-    std::fstream gdata;
-    gdata.open ("/home/buyus/Escritorio/velocityProfile.csv", std::fstream::out);
+//    std::fstream gdata;
+//    gdata.open ("/home/buyus/Escritorio/velocityProfile.csv", std::fstream::out);
 
 
-    double lastJointPos,jointPos;
-    int repeat=1;
-    lastJointPos=robot.GetJoint(jointNumber);
+//    double lastJointPos,jointPos;
+//    int repeat=1;
+//    lastJointPos=robot.GetJoint(jointNumber);
 
-    double actualVel;
-    robot.SetJointVel(jointNumber, vel);
-
-
-    for(int i=0; i<loops; i++)
-    {
-        jointPos=robot.GetJoint(jointNumber);
-
-        pos.push_back(jointPos);
-
-        if (jointPos==lastJointPos)
-        {
-            repeat++;
-        }
-        else
-        {
-     /*   lastTime = actualTime; //now actualTime is not actual but last
-        actualTime = ((float)clock())/CLOCKS_PER_SEC;
-        elapsedTime = actualTime-lastTime;
-        totalTime +=elapsedTime;*/
-        totalTime += Ts*repeat;
-        actualVel = (jointPos-lastJointPos)/(Ts*repeat);
-
-        //fprintf (gdata, "%f - %f - %f - %f \n",Ts*i,vel,actualVel,jointPos);
-        gdata << totalTime << " - "
-                 << repeat << " - "
-              << vel << " - "
-              << actualVel << " - "
-              << jointPos << " "
-              << std::endl;
+//    double actualVel;
+//    robot.SetJointVel(jointNumber, vel);
 
 
-        lastJointPos = jointPos;
-        repeat=1;
-        }
+//    for(int i=0; i<loops; i++)
+//    {
+//        jointPos=robot.GetJoint(jointNumber);
+
+//        pos.push_back(jointPos);
+
+//        if (jointPos==lastJointPos)
+//        {
+//            repeat++;
+//        }
+//        else
+//        {
+//     /*   lastTime = actualTime; //now actualTime is not actual but last
+//        actualTime = ((float)clock())/CLOCKS_PER_SEC;
+//        elapsedTime = actualTime-lastTime;
+//        totalTime +=elapsedTime;*/
+//        totalTime += Ts*repeat;
+//        actualVel = (jointPos-lastJointPos)/(Ts*repeat);
+
+//        //fprintf (gdata, "%f - %f - %f - %f \n",Ts*i,vel,actualVel,jointPos);
+//        gdata << totalTime << " - "
+//                 << repeat << " - "
+//              << vel << " - "
+//              << actualVel << " - "
+//              << jointPos << " "
+//              << std::endl;
 
 
-        //std::chrono::high_resolution_clock()
-        yarp::os::Time::delay(Ts);
+//        lastJointPos = jointPos;
+//        repeat=1;
+//        }
 
 
-    }
-   //step =0;
-    robot.SetJointVel(jointNumber, 0);
+//        //std::chrono::high_resolution_clock()
+//        yarp::os::Time::delay(Ts);
 
 
-    gdata.close();
-}
+//    }
+//   //step =0;
+//    robot.SetJointVel(jointNumber, 0);
+
+
+//    gdata.close();
+//}
 
